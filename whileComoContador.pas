3c0c0d10e8694bf5f0fa9e7362e35a22dcc4c885
Juan Beltrame <61248615+JuanBeltrame@users.noreply.github.com>
2022-05-12 17:44:00 -0300

    Add files via upload

---
+++ whileComoContador.pas
@@ -0,0 +1,13 @@
+
+Program whileComoContador;
+
+Var contador: integer;
+
+Begin
+  contador := 0;
+  While contador <50 Do
+    Begin
+      contador := contador+1;
+      WriteLn(contador);
+    End;
+End.
